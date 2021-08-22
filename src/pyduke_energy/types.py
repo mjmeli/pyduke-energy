2f32097cfd5886b19f86c2c89865c8a3ad136ab6
Michael Meli <mjmeli94@gmail.com>
2021-08-22 13:33:22 -0400

    add type hints, remove asyncio as requirement

--- src/pyduke_energy/types.py
+++ src/pyduke_energy/types.py
@@ -1,6 +1,7 @@
 """Type Defintions"""
 
 """An account as provided by account-list endpoint"""
+from datetime import datetime, date
 from pyduke_energy.utils import (
     str_to_datetime,
     str_to_date,
@@ -9,74 +10,74 @@ from pyduke_energy.utils import (
 
 class Account:
     def __init__(self, data: dict):
-        self.default_account = data.get("defaultAccount")
-        self.nickname = data.get("nickname")
-        self.account_number = data.get("accountNumber")
-        self.src_acct_id = data.get("srcAcctId")
-        self.src_sys_cd = data.get("srcSysCd")
-        self.status = data.get("status")
-        self.role = data.get("role")
-        self.service_address = data.get("serviceAddress")
-        self.mobile_app_compatible = data.get("mobileAppCompatible")
+        self.default_account: bool = data.get("defaultAccount")
+        self.nickname: str = data.get("nickname")
+        self.account_number: str = data.get("accountNumber")
+        self.src_acct_id: str = data.get("srcAcctId")
+        self.src_sys_cd: str = data.get("srcSysCd")
+        self.status: str = data.get("status")
+        self.role: str = data.get("role")
+        self.service_address: str = data.get("serviceAddress")
+        self.mobile_app_compatible: bool = data.get("mobileAppCompatible")
 
 """Details account data as provided by account-details endpoint"""
 class AccountDetails:
     def __init__(self, data: dict):
-        self.customer_name = data.get("customerName")
-        self.first_name = data.get("firstName")
-        self.last_name = data.get("lastName")
-        self.mailing_address = MailingAddress(data.get("mailingAddress"))
-        self.service_address = ServiceAddress(data.get("serviceAddress"))
-        self.is_electric = data.get("isElectric")
-        self.is_gas = data.get("isGas")
-        self.meter_infos = [MeterInfo(mi) for mi in data.get("meterInfo")]
+        self.customer_name: str = data.get("customerName")
+        self.first_name: str = data.get("firstName")
+        self.last_name: str = data.get("lastName")
+        self.mailing_address: MailingAddress = MailingAddress(data.get("mailingAddress"))
+        self.service_address: ServiceAddress = ServiceAddress(data.get("serviceAddress"))
+        self.is_electric: bool = data.get("isElectric")
+        self.is_gas: bool = data.get("isGas")
+        self.meter_infos: list[MeterInfo] = [MeterInfo(mi) for mi in data.get("meterInfo")]
 
 """Base address data"""
 class BaseAddress:
     def __init__(self, data: dict):
-        self.address_line_1 = data.get("addressLine1")
-        self.address_line_2 = data.get("addressLine2")
-        self.city = data.get("city")
-        self.state = data.get("stateCode")
-        self.zip_code = data.get("zipCode")
+        self.address_line_1: str = data.get("addressLine1")
+        self.address_line_2: str = data.get("addressLine2")
+        self.city: str = data.get("city")
+        self.state: str = data.get("stateCode")
+        self.zip_code: str = data.get("zipCode")
 
 """Mailing address data as provided by account-details endpoint"""
 class MailingAddress(BaseAddress):
     def __init__(self, data: dict):
-        self.address_number = data.get("addressNumber")
         super().__init__(data)
+        self.address_number: str = data.get("addressNumber")
 
 """Service address data as provided by account-details endpoint"""
 class ServiceAddress(BaseAddress):
     def __init__(self, data: dict):
-        self.premise_id = data.get("premiseID")
         super().__init__(data)
+        self.premise_id: str = data.get("premiseID")
 
 """Meter data as provided by account-details endpoint"""
 class MeterInfo:
     def __init__(self, data: dict):
-        self.meter_type = data.get("meterType")
-        self.serial_num = data.get("serialNum")
-        self.agreement_active_date = str_to_date(data.get("agreementActiveDate")) # this is specifically a date with no time component
-        self.is_certified_smart_meter = data.get("isCertifiedSmartMeter")
-        self.op_center = data.get("opCenter")
-        self.service_id = data.get("serviceId")
-        self.transformer_number = data.get("transformerNumber")
+        self.meter_type: str = data.get("meterType")
+        self.serial_num: str = data.get("serialNum")
+        self.agreement_active_date: date = str_to_date(data.get("agreementActiveDate")) # this is specifically a date with no time component
+        self.is_certified_smart_meter: bool = data.get("isCertifiedSmartMeter")
+        self.op_center: str = data.get("opCenter")
+        self.service_id: str = data.get("serviceId")
+        self.transformer_number: str = data.get("transformerNumber")
 
 """Gateway status data as provided by gateways/status endpoint"""
 class GatewayStatus:
     def __init__(self, data: dict):
-        self.id = data.get("_id")
-        self.service_state = data.get("serviceState")
-        self.service_date = str_to_datetime(data.get("serviceDt"))
-        self.connected = data.get("connected")
-        self.connect_date = str_to_datetime(data.get("connectTm"))
-        self.mac_address = data.get("gwMAC")
-        self.zigbee_mac_address = data.get("zgbMAC")
+        self.id: str = data.get("_id")
+        self.service_state: str = data.get("serviceState")
+        self.service_date: datetime = str_to_datetime(data.get("serviceDt"))
+        self.connected: bool = data.get("connected")
+        self.connect_date: datetime = str_to_datetime(data.get("connectTm"))
+        self.mac_address: str = data.get("gwMAC")
+        self.zigbee_mac_address: str = data.get("zgbMAC")
 
 """Usage reading from the usageByHour endpoint"""
 class UsageMeasurement:
     def __init__(self, data: dict):
-        self.timestamp = data.get("t") / 1000 # remove ms
-        self.datetime_utc = utc_timestamp_to_datetime(self.timestamp)
-        self.usage = data.get("i") # i works for now
+        self.timestamp: int = int(data.get("t") / 1000) # remove ms
+        self.datetime_utc: datetime = utc_timestamp_to_datetime(self.timestamp)
+        self.usage: float = data.get("i") # i works for now
